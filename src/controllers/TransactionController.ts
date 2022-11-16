0e8172533f5a8befb1bd6f04d8d2c315b36ba3f3
gustas01 <gustasboyevolution@hotmail.com>
2022-11-16 15:09:11 -0300

    implementando read das transactions para o usuário logado

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,4 +1,5 @@
 import Transaction from '../models/Transaction'
+import { Op } from 'sequelize'
 
 class TransactionController {
   async create(debited_account_id: number, credited_account_id: number, value: number){
@@ -8,12 +9,25 @@ class TransactionController {
       
       return transaction
     }catch(err){
-
+      return new Error('Falha ao criar transação')
     }
   }
 
   async read(req: any, res: any){
-
+    try{
+      const transactions = await Transaction.findAll({where: {
+        [Op.or]: [
+          {debited_account_id: req.user_id},
+          {credited_account_id: req.user_id}
+        ]
+      }})
+      
+      return res.status(200).json({transactions})
+    }catch(error: any){ 
+      return res.status(400).json({
+        errors: error.errors?.map(err => err.message)
+      })
+    }
   }
 
   async update(req, res){
