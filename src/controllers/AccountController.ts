6c9b6aa451bc99f3a59e86562ea196c6107ce83f
gustas01 <gustasboyevolution@hotmail.com>
2022-11-18 18:55:07 -0300

    adicionando validação de valor não enviado de cashout

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -28,23 +28,28 @@ class AccountController {
   async update(req: any, res: any){
     //quando fizer cash-in e cash-out
     try{
+      if(!req.body.cashOutValue)
+        return res.status(400).json({
+          errors: ['Valor não enviado']
+        })
+        
       const account = await Account.findByPk(req.account_id)
       
       if(account?.dataValues.balance < req.body.cashOutValue)
-        return res.status(401).json({
+        return res.status(400).json({
           errors: ['Saldo insuficiente']
       })
       
       //verificar se o usuário não está mandando para ele mesmo
       if(req.user_name === req.body.user_name_cashIn)
-        return res.status(401).json({
+        return res.status(400).json({
           errors: ["Não é possível fazer cash-out para si mesmo"]
         })
 
       const userCashIn = await User.findOne({where: {user_name: req.body.user_name_cashIn}})
       
       if(!userCashIn)
-        return res.status(401).json({
+        return res.status(400).json({
           errors: ["Não foi possível encontrar usuário para cash-in"]
       })
       
@@ -57,8 +62,8 @@ class AccountController {
 
       TransactionController.create(req.account_id, userCashIn.dataValues.account_id, req.body.cashOutValue)
 
-      return res.status(200).send(
-        `Cash-out de ${req.body.cashOutValue}R$ realizado com sucesso para ${req.body.user_name_cashIn}`
+      return res.status(200).json(
+        {msg: `Cash-out de ${req.body.cashOutValue}R$ realizado com sucesso para ${req.body.user_name_cashIn}`}
       )
     }catch(error: any){
       return res.status(200).json({
